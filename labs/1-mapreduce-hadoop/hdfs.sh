f9445c0efba79bb793d51866541c5f8ae5558d79
Achintya Kumar <achintya-kumar@users.noreply.github.com>
2017-04-23 02:11:20 +0200

    Creating 'target' folder beforehand for result is unnecessary as it results in exception.

--- labs/1-mapreduce-hadoop/hdfs.sh
+++ labs/1-mapreduce-hadoop/hdfs.sh
@@ -3,6 +3,3 @@ hadoop fs -mkdir source
 
 #Copying a downloaded text file from gutenberg.org to the source folder inside HDFS
 hadoop fs -put MarinerMissionToMars.txt source/MarinerMissionToMars.txt
-
-#Also creating another folder for storing results of map-reduce computations later
-hadoop fs -mkdir target
