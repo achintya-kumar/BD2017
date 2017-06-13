dc79852d73d4ca62a5fb5b5fea2d25efd4bc80eb
Achintya Kumar <achintya-kumar@users.noreply.github.com>
2017-06-13 03:13:15 +0200

    Create UnlimitedJCEPolicyJDK7.sh

---
+++ labs/4-security/Personal/UnlimitedJCEPolicyJDK7.sh
@@ -0,0 +1,8 @@
+#Place this download inside /root
+
+wget \
+    --no-cookies \
+    --no-check-certificate \
+    --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+    -O UnlimitedJCEPolicyJDK7.zip \
+    http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip
